af2568b836ddb0c334f1191662975ab3530495b4
QuodImperator <darkostefanoski951@gmail.com>
2024-10-04 18:58:37 +0200

    GraphQL testing

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -13,10 +13,10 @@ class Connection
 
     private function __construct()
     {
-        $host = $_ENV['DB_HOST'];
-        $db   = $_ENV['DB_NAME'];
-        $user = $_ENV['DB_USER'];
-        $pass = $_ENV['DB_PASS'];
+        $host = $_ENV['DB_HOST'] ?? 'localhost';
+        $db   = $_ENV['DB_NAME'] ?? 'scandi_base';
+        $user = $_ENV['DB_USER'] ?? 'root';
+        $pass = $_ENV['DB_PASS'] ?? '';
         $charset = 'utf8mb4';
 
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
