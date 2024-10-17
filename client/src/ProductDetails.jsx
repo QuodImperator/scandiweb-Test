a0e7b3d4db132619b6e890a8a87e85f94d647dba
QuodImperator <darkostefanoski951@gmail.com>
2024-10-17 17:19:07 +0200

    Finishing touches on PDP

--- client/src/ProductDetails.jsx
+++ client/src/ProductDetails.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Query } from '@apollo/client/react/components';
 import { gql } from '@apollo/client';
 import { useParams } from 'react-router-dom';
-import ImageGallery from 'react-image-gallery';
-import 'react-image-gallery/styles/css/image-gallery.css';
 
 const GET_PRODUCT = gql`
   query GetProduct($id: String!) {
@@ -41,7 +39,9 @@ class ProductDetailsContent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedAttributes: {}
+      selectedAttributes: {},
+      selectedImageIndex: 0,
+      isHoveringImage: false
     };
   }
 
@@ -54,27 +54,57 @@ class ProductDetailsContent extends React.Component {
     }));
   };
 
+  handleThumbnailClick = (index) => {
+    this.setState({ selectedImageIndex: index });
+  };
+
+  handlePrevImage = () => {
+    this.setState(prevState => ({
+      selectedImageIndex: (prevState.selectedImageIndex - 1 + this.props.product.gallery.length) % this.props.product.gallery.length
+    }));
+  };
+
+  handleNextImage = () => {
+    this.setState(prevState => ({
+      selectedImageIndex: (prevState.selectedImageIndex + 1) % this.props.product.gallery.length
+    }));
+  };
+
+  handleImageHover = (isHovering) => {
+    this.setState({ isHoveringImage: isHovering });
+  }
+
   render() {
     const { product } = this.props;
-    const images = product.gallery.map(image => ({
-      original: image,
-      thumbnail: image
-    }));
+    const { selectedImageIndex } = this.state;
 
     return (
       <div className="product-details">
-        <div className="product-gallery" data-testid="product-gallery">
-          <ImageGallery 
-            items={images}
-            showPlayButton={false}
-            showFullscreenButton={false}
-          />
+        <div className="product-gallery">
+          <div className="thumbnails">
+            {product.gallery.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`${product.name} thumbnail ${index + 1}`}
+                className={`thumbnail ${selectedImageIndex === index ? 'selected' : ''}`}
+                onClick={() => this.handleThumbnailClick(index)}
+              />
+            ))}
+          </div>
+          <div className="main-image">
+            <img src={product.gallery[selectedImageIndex]} alt={product.name} />
+            <button className="image-scroll-button left" onClick={this.handlePrevImage}>&lt;</button>
+            <button className="image-scroll-button right" onClick={this.handleNextImage}>&gt;</button>
+          </div>
         </div>
         <div className="product-info">
           <h1 className="product-name">{product.name}</h1>
+          <h2 className="product-brand">{product.brand}</h2>
+
           {product.attributes.map(attribute => (
             <div key={attribute.id} className="product-attribute">
-              <h3>{attribute.name}</h3>
+              <h3>{attribute.name}:</h3>
               <div className="attribute-options">
                 {attribute.items.map(item => (
                   <button
@@ -89,27 +119,25 @@ class ProductDetailsContent extends React.Component {
               </div>
             </div>
           ))}
-          
+
           <div className="product-price">
-            <h3>Price</h3>
+            <h3>PRICE:</h3>
             {product.prices.map((price, index) => (
               <p key={index}>{price.currency.symbol}{price.amount.toFixed(2)}</p>
             ))}
           </div>
-          
+
           <button
             className="add-to-cart"
             disabled={!product.inStock}
             onClick={() => console.log('Add to cart:', product.id, this.state.selectedAttributes)}
-            data-testid="add-to-cart"
           >
-            {product.inStock ? 'Add to Cart' : 'Out of Stock'}
+            {product.inStock ? 'ADD TO CART' : 'OUT OF STOCK'}
           </button>
-          
-          <div 
-            className="product-description" 
-            data-testid="product-description"
-            dangerouslySetInnerHTML={{ __html: product.description }} 
+
+          <div
+            className="product-description"
+            dangerouslySetInnerHTML={{ __html: product.description }}
           />
         </div>
       </div>
@@ -120,7 +148,7 @@ class ProductDetailsContent extends React.Component {
 class ProductDetailsClass extends React.Component {
   render() {
     const { id } = this.props;
-    
+
     return (
       <Query query={GET_PRODUCT} variables={{ id }}>
         {({ loading, error, data }) => {
