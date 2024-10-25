c7bc4522723adaef647cfc1b14405723a0bf9ba9
QuodImperator <darkostefanoski951@gmail.com>
2024-10-25 20:27:26 +0200

    Reverted WithRouter

--- client/src/WithRouter.jsx
+++ client/src/WithRouter.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
 
-const RouterWrapper = ({ Component, ...props }) => {
-  const params = useParams();
-  const navigate = useNavigate();
-  const location = useLocation();
-  
-  return <Component {...props} router={{ params, navigate, location }} />;
-};
-
-class WithRouter extends React.Component {
-  render() {
-    const Component = this.props.WrappedComponent;
-    return <RouterWrapper Component={Component} {...this.props} />;
-  }
-}
-
-export function withRouter(WrappedComponent) {
-  return class extends React.Component {
-    render() {
-      return <WithRouter WrappedComponent={WrappedComponent} {...this.props} />;
-    }
+function WithRouterWrapper(Component) {
+  return function ComponentWithRouter(props) {
+    let params = useParams();
+    let navigate = useNavigate();
+    let location = useLocation();
+    
+    return (
+      <Component
+        {...props}
+        router={{ params, navigate, location }}
+      />
+    );
   };
 }
 
-export { WithRouter };
+export function WithRouter(WrappedComponent) {
+  return WithRouterWrapper(WrappedComponent);
+}
+
+export default WithRouter;
