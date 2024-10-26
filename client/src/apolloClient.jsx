a658902a48480e5d9c16a0bec792a82b0b5b3a72
QuodImperator <darkostefanoski951@gmail.com>
2024-10-26 20:28:44 +0200

    Preparing for deployment

--- client/src/apolloClient.jsx
+++ client/src/apolloClient.jsx
@@ -1,9 +1,13 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
+    // For local development
     uri: 'http://localhost:8080/graphql',
-    credentials: 'same-origin'
-  });
+    credentials: 'include',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
 
 const client = new ApolloClient({
     link: httpLink,
--- public/index.php
+++ public/index.php
@@ -1,22 +1,23 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Credentials: true");
-
-ini_set('display_errors', 0);
-error_reporting(E_ALL);
-ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/../php-error.log');
 
 // Load environment variables
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
-// The rest of your application code...
+// Basic CORS headers
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
 
-$dispatcher = FastRoute\simpleDispatcher(function (FastRoute\RouteCollector $r) {
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
     $r->addRoute(['GET', 'POST'], '/graphql', [App\Controller\GraphQLController::class, 'handle']);
 });
 
@@ -27,9 +28,11 @@ $routeInfo = $dispatcher->dispatch(
 
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
+        header('HTTP/1.0 404 Not Found');
         echo '404 Not Found';
         break;
     case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        header('HTTP/1.0 405 Method Not Allowed');
         echo '405 Method Not Allowed';
         break;
     case FastRoute\Dispatcher::FOUND:
@@ -38,8 +41,5 @@ switch ($routeInfo[0]) {
         [$controller, $method] = $handler;
         $controllerInstance = new $controller();
         echo $controllerInstance->$method($vars);
-        break;
-    default:
-        echo 'An error occurred.';
         break;
 }
