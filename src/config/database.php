ec6694921e0ba024dfa879e68eed7854a15f50da
QuodImperator <darkostefanoski951@gmail.com>
2024-10-26 20:12:39 +0200

    Connection config

--- src/config/database.php
+++ src/config/database.php
@@ -1,9 +1,14 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'dbname' => 'scandi_base',
-    'user' => 'scandi_user',
-    'password' => '951951',
+    'host' => $_ENV['DB_HOST'] ?? 'localhost',
+    'dbname' => $_ENV['DB_NAME'] ?? 'scandi_base',
+    'user' => $_ENV['DB_USER'] ?? 'root',
+    'password' => $_ENV['DB_PASS'] ?? '',
     'charset' => 'utf8mb4',
+    'options' => [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]
 ];
